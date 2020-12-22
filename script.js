9786431e33ba37c4d5c55c19c9df78a3ccd9b061
Ariel Snow <arielbrizuelak@gmail.com>
2020-12-21 23:22:40 -0800

    Made it so the intro and start button would add a hide function once clicked on

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const bodyIntro = document.getElementById('body-intro')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
 
 function startGame() {
     startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
+    bodyIntro.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    question.classList.remove('hide')
+    answerButtonsElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        Question:'What is 2+2',
+        answers: [
+            {text:'4', correct: true },
+            {text:'fish', correct:false}
+        ]
+    }
+]
